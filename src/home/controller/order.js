2e9710a093f9f793484ed6e34c5d8abf0aefe3b8
YiKeYaTu <liangchen005005@163.com>
2016-08-10 11:58:40 +0800

    api test

--- src/home/controller/order.js
+++ src/home/controller/order.js
@@ -30,6 +30,8 @@ export default class extends Base {
 
         let http = this.http;
 
+        console.log(http.host + http.url);
+
         let userInf = await wx.getUserInf(
             false,
             http, 
