2b4473ba20e378c68daf7bc6ca7aa064dcd0e51c
YiKeYaTu <liangchen005005@163.com>
2016-08-15 15:18:38 +0800

    test

--- src/home/controller/order.js
+++ src/home/controller/order.js
@@ -57,7 +57,7 @@ export default class extends Base {
 
     async payAction () {
 
-        let userInf = await this,
+        let userInf = await this.checkUserInf(),
             jsTicket = await this.checkJsSdk();
 
         let orderId = this.get('order_id');
@@ -65,7 +65,7 @@ export default class extends Base {
         if (!userInf) return this.fail(10001);
         // let 
 
-        let openid = 'oDNUjwV7l6KYEaEaBlWWSSn4Nel4' /*userInf.openid*/;
+        let openid = /*'oDNUjwV7l6KYEaEaBlWWSSn4Nel4'*/ userInf.openid;
 
         let orderModel = this.model('order'),
             adressModel = this.model('adress');
