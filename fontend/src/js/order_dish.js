cbc3b728a0f11de5e621cce0b80ccd8b1039285e
duwonders <duwonders@gmail.com>
2016-08-20 21:02:54 +0800

    dsa

--- fontend/src/js/order_dish.js
+++ fontend/src/js/order_dish.js
@@ -129,31 +129,31 @@ let orderKeeper = (function () {
 
     $('.kouwei-con').on('click', function(e){
 
-        $('.kouwei-con').children().map(function(i){
-            $(i).css({color: '#fff', background: 'rgb(204, 172, 134)'})
-        })
+        $('.kouwei-con').children().map(function(i, item){
 
-        $(e.target).css({color: '#cc9933', background: '#fff'})
+            $(item).css({color: '#cc9933', background: '#fff'})
 
-        alert(1);
+        })  
+
+        $(e.target).css({color: '#fff', background: '#cc9933'})
 
     })
 
     $('.add').on('click', function(){
 
-        $('.no-border').innerHTML = parseInt( $('.no-border').innerHTML ) + 1;
+        if($('.no-border')[0].innerHTML > 0){
+            
+            $('.no-border')[0].innerHTML = parseInt( $('.no-border')[0].innerHTML ) - 1;
+        }
 
     });
     
     $('.app').on('click', function(){
 
-        if(parseInt($('.no-border').innerHTML) > 0){
-            alert(1);
-            console.log($('.no-border').innerHTML);
-            console.log(parseInt( $('.no-border').innerHTML ));
-            $('.no-border').innerHTML = parseInt( $('.no-border').innerHTML ) - 1;
+        
+            
+            $('.no-border')[0].innerHTML = parseInt( $('.no-border')[0].innerHTML ) + 1;
 
-        }      
     });
 }());
 
