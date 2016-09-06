6f4b9df75158e60b7fd2d7fa76776ee4a94a05be
duwonders <duwonders@gmail.com>
2016-09-06 15:14:29 +0800

    dsa

---
+++ src/common/config/wx.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * wx configs
+ */
+export default {
+  appid: 'wxed1c9924e6340f5e',
+  appSecret: '656f2fc1a74d07cb44b9604608d759ba',
+  mchId: '1340077601',
+  key: '12345678123456781234567812345678'
+};
